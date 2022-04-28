672750d3e34c15548fd45d3de76473bf48554609
Rob Sawyer <robksawyer@gmail.com>
2022-04-28 20:43:39 +0900

    Updated the three version

--- build/three-noise.js
+++ build/three-noise.js
@@ -10,14 +10,12 @@ var THREE_Noise = (function (exports, THREE) {
           var d = Object.getOwnPropertyDescriptor(e, k);
           Object.defineProperty(n, k, d.get ? d : {
             enumerable: true,
-            get: function () {
-              return e[k];
-            }
+            get: function () { return e[k]; }
           });
         }
       });
     }
-    n['default'] = e;
+    n["default"] = e;
     return Object.freeze(n);
   }
 
@@ -588,4 +586,4 @@ var THREE_Noise = (function (exports, THREE) {
 
   return exports;
 
-}({}, THREE));
+})({}, THREE);
